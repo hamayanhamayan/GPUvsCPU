c5b65ad8f0630a33736b425d2353f4d540b81a2f
hamayanhamayan <bz.programmer@gmail.com>
2017-05-25 15:42:46 +0900

    Debug

--- gpu.cu
+++ gpu.cu
@@ -4,22 +4,22 @@
 
 #define SHARED_MEMORY_MAX 10000
 __global__ void kernel(int *buf, int len) {
-    __shared__ int sm;
+    __shared__ int sm[SHARED_MEMORY_MAX];
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
     if(len <= tid) return;
 
-    if(tid == 0) sm = 0;
+    if(tid == 0) sm[0] = 0;
 
     __syncthreads();
 
-    sm += buf[tid];
+    atomicAdd(sm, buf[tid]);
 
     __syncthreads();
 
-    if(tid == 0) buf[0] = sm;
+    if(tid == 0) buf[0] = sm[0];
 }
 
-#define NUM_BLOCKS 1024
+#define NUM_BLOCKS 1
 #define NUM_THREADS 512
 
 int gpu(const int* buf, int len) {
@@ -32,8 +32,8 @@ int gpu(const int* buf, int len) {
 	kernel<<<NUM_BLOCKS, NUM_THREADS>>>(gbuf, len);
 
 	int* nbuf = (int*)malloc(sizeof(int) * len);
-	
 	cudaMemcpy(nbuf, gbuf, sizeof(int) * len, cudaMemcpyDeviceToHost);
+
 	int result = nbuf[0];
 
 	cudaFree(gbuf);
